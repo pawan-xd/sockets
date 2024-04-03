cab0c5082236f51461a72c1ff7d030cb6dbb63c4
Pawan <pawanyadav020202@gmail.com>
2024-04-04 01:19:48 +0530

    8.6

--- views/chatHandler.js
+++ views/chatHandler.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("wss://192.168.0.115:3001");
+const ws = new WebSocket("wss://localhost:3001");
 
 // ws.addEventListener("message", (event) => {
 //     console.log("Received:", event.data);
